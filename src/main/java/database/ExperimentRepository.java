6b2ebdcb257f571640e26001dde1cc3108f950c0
mdtokarev277 <mdtokarev277@gmail.com>
2026-05-21 18:42:36 +0300

    add RunRepository.java - for work with runs table from DB

--- src/main/java/database/ExperimentRepository.java
+++ src/main/java/database/ExperimentRepository.java
@@ -7,6 +7,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+// класс работает с таблицей experiments из БД
 public class ExperimentRepository {
     private  final Database database;
 
